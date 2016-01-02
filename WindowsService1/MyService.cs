41cd3353196d0677a00aa422c38031f92c4dac89
Aleksandrs K. <aleksandrs.kass@gmail.com>
2016-01-03 01:06:23 +0200

    Windows servisa samplis ar consoles dabug variantu

---
+++ WindowsService1/MyService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+using System.ServiceProcess;
+
+namespace WindowsService1
+{
+    public partial class MyService : ServiceBase
+    {
+        private System.Timers.Timer _timer;
+        private bool _timerTaskSuccess;
+
+        public MyService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            try
+            {
+                //
+                // Create and start a timer.
+                //
+                _timer = new System.Timers.Timer {Interval = 60000}; // every one min
+                _timer.Elapsed += _timer_Elapsed;
+                _timer.AutoReset = false;  // makes it fire only once
+                _timer.Start(); // Start
+
+                _timerTaskSuccess = false;
+            }
+            catch (Exception ex)
+            {
+                // omitted
+            }
+        }
+
+        private void _timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                WriteMessage(DateTime.Now.ToString(CultureInfo.InvariantCulture));
+
+                _timerTaskSuccess = true;
+            }
+            catch (Exception ex)
+            {
+                _timerTaskSuccess = false;
+            }
+            finally
+            {
+                if (_timerTaskSuccess)
+                {
+                    _timer.Start();
+                }
+            }
+        }
+
+        public void WriteMessage(string message)
+        {
+            
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                // Service stopped. Also stop the timer.
+                _timer.Stop();
+                _timer.Dispose();
+                _timer = null;
+            }
+            catch (Exception ex)
+            {
+                // omitted
+            }
+        }
+    }
+}
---
+++ WindowsService1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceProcess;
+
+namespace WindowsService1
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            if (!Environment.UserInteractive)
+            {
+                var servicesToRun = new ServiceBase[]
+                {
+                    new MyService()
+                };
+                ServiceBase.Run(servicesToRun);
+            }
+            else
+            {
+                var myService = new MyService();
+                myService.WriteMessage("Debug");
+                Console.WriteLine("Service start...");
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
