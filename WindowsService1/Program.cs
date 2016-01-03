de0feacaa307549a5ed894b8d38393c740e9534a
Aleksandrs K. <aleksandrs.kass@gmail.com>
2016-01-03 22:09:39 +0200

    static Logger member

--- WindowsService1/Program.cs
+++ WindowsService1/Program.cs
@@ -7,6 +7,17 @@ namespace WindowsService1
 {
     static class Program
     {
+        private static log4net.ILog _logger;
+        private static log4net.ILog Logger
+        {
+            get
+            {
+                if (_logger != null) return _logger;
+                _logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+                return _logger;
+            }
+        }
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -15,8 +26,8 @@ namespace WindowsService1
             //log4net.Config.BasicConfigurator.Configure();
             //log4net.Config.XmlConfigurator.Configure();
 
-            var logger = log4net.LogManager.GetLogger(typeof (Program));
-            logger.Info("Hello from log4net!");
+            //var logger = log4net.LogManager.GetLogger(typeof (Program));
+            Logger.Info("Hello from log4net!");
 
             if (!Environment.UserInteractive)
             {
